e8c3eb9dd96d9632bf61312ed403af3662a7902e
ryabenbc <ryaben@bettercollective.com>
2020-12-01 18:51:52 +0100

    Removed the network to include some health checks on the db, so it can be set up before the main application starts running.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
-python -u -m director
+# Check if the database is postgresql, and if so, wait for it to be ready
+if [ "$DATABASE" = "test_health_before" ]
+then
+  echo "Waiting for postgres..."
+  nc -z postgres 5432
+  while ! nc -z postgres 5432; do
+    sleep 0.1
+  done
+
+  echo "PostgreSQL started"
+fi
+
+#python -u -m director
 
 exec "$@"
